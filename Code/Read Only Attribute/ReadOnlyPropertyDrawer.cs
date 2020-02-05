d08773b22f0c70fb08b051d6361d1394f643f667
Comp-3 Interactive <Comp3interactive@users.noreply.github.com>
2020-02-05 12:02:29 +0000

    Initial Commit

---
+++ Code/Read Only Attribute/ReadOnlyPropertyDrawer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyPropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label);
+        GUI.enabled = true;
+    }
+}
